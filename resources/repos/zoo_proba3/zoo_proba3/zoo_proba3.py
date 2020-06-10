fdc7d5d0c6b9ecb7f01bac3aaec39445d818f8a0
perkele4040 <perkele4040@DESKTOP-K5T93A4>
2020-06-10 18:10:38 +0200

    sql connection maybe working

--- resources/repos/zoo_proba3/zoo_proba3/zoo_proba3.py
+++ resources/repos/zoo_proba3/zoo_proba3/zoo_proba3.py
@@ -1,6 +1,18 @@
-import wpf
+import wpf, clr
+
+clr.AddReference('System.Data')
+from System.Data import SqlClient
 
 from System.Windows import Application, Window
+
+conn = SqlClient.SqlConnection("Server=sql7.freemysqlhosting.net;Database=sql7343753;UID=sql7343753;PWD=JGWVXubSX1")
+conn.Open()
+
+cmd = SqlClient.SqlCommand(sql, conn)
+exe = cmd.ExecuteReader()
+
+exe.Close()
+conn.Close()
 
 class MyWindow(Window):
     def __init__(self):
