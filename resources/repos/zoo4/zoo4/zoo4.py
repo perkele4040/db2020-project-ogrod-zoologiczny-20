2bf6e552caeb6785952a82db9e6141e7d61a0926
perkele4040 <perkele4040@DESKTOP-K5T93A4>
2020-06-10 22:14:03 +0200

    reloading project to fix project #3

--- resources/repos/zoo4/zoo4/zoo4.py
+++ resources/repos/zoo4/zoo4/zoo4.py
@@ -1,11 +1,20 @@
 import wpf
-
+import clr
+clr.AddReference('System.Data')
+from System.Data import SqlClient
 from System.Windows import Application, Window
+
+conn = SqlClient.SqlConnection("Server=sql7.freemysqlhosting.net;Database=sql7343753;UID=sql7343753;PWD=JGWVXubSX1")
+conn.Open()
 
 class MyWindow(Window):
     def __init__(self):
         wpf.LoadComponent(self, 'zoo4.xaml')
-    
+
+        
+    def Button_Click(self, sender, e):
+        pass
+
 
 if __name__ == '__main__':
     Application().Run(MyWindow())
